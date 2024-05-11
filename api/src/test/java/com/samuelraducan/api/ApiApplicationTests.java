412155dbef2c35d2442fb97d46af4fd18e49c7d2
Samuel Raducan <samuel.thembp@gmail.com>
2024-05-11 10:00:33 +0100

    Added spring project

---
+++ api/src/test/java/com/samuelraducan/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuelraducan.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
