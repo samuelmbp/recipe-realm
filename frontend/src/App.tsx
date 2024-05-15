60c55cf85c6acfecf1e09038a5b41e876e4ea354
Samuel Raducan <samuel.thembp@gmail.com>
2024-05-15 09:56:55 +0100

    Added navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Home from "./containers/Home/Home";
 import ViewRecipes from "./containers/ViewRecipes/ViewRecipes";
+import Nav from "./components/Nav/Nav";
 
 function App() {
     return (
         <BrowserRouter>
+            <Nav />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/recipes" element={<ViewRecipes />} />
---
+++ frontend/src/components/Nav/Nav.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import "./Nav.scss";
+
+const Nav = () => {
+    return (
+        <div className="nav">
+            <Link className="nav__item" to="/">
+                Home
+            </Link>
+
+            <Link className="nav__item" to="/recipes">
+                Recipes
+            </Link>
+
+            <Link className="nav__item" to="/greeting/create">
+                Add Recipe
+            </Link>
+        </div>
+    );
+};
+
+export default Nav;
