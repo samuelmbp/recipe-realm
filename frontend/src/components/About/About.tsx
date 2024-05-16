abbc66d791261fd94e2a17c5c27af7aa39dfffa0
Samuel Raducan <samuel.thembp@gmail.com>
2024-05-16 17:06:18 +0100

    Added about section

---
+++ frontend/src/components/About/About.tsx
@@ -0,0 +1,33 @@
+import "./About.scss";
+
+const About = () => {
+    return (
+        <section className="about">
+            <div className="about__content">
+                <h2 className="about__title">Welcome to Recipe Realm!</h2>
+                <p className="about__text">
+                    At Recipe Realm, we're passionate about bringing you the
+                    most delicious and mouthwatering recipes from around the
+                    globe. Whether you're a seasoned chef or a cooking novice,
+                    our diverse collection of recipes has something for
+                    everyone.
+                </p>
+                <p className="about__text">
+                    Our team of culinary experts works tirelessly to curate and
+                    create recipes that are not only easy to follow but also
+                    guaranteed to impress your family and friends. From quick
+                    and easy weeknight dinners to indulgent desserts, we've got
+                    you covered.
+                </p>
+                <p className="about__text">
+                    Join our culinary journey and discover the joy of cooking
+                    with Recipe Realm. Get ready to embark on a flavorful
+                    adventure that will tantalize your taste buds and inspire
+                    your inner chef!
+                </p>
+            </div>
+        </section>
+    );
+};
+
+export default About;
--- frontend/src/containers/Home/Home.tsx
+++ frontend/src/containers/Home/Home.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
 import heroImage from "../../assets/hero-image.jpg";
+import RecipeResponse from "../../types/RecipeResponse";
 import "./Home.scss";
+import RecipeList from "../../components/RecipeList/RecipeList";
+import About from "../../components/About/About";
 
 const Home = () => {
+    const [recipes, setRecipes] = useState<RecipeResponse[]>([]);
+
+    const getRecipes = async () => {
+        const response = await fetch("http://localhost:8081/recipes");
+        const recipeData: RecipeResponse[] = await response.json();
+        const firstThreeRecipes = recipeData.slice(0, 3);
+        setRecipes(firstThreeRecipes);
+    };
+
+    useEffect(() => {
+        getRecipes();
+    }, []);
+
     return (
         <div className="home">
             <div className="home-hero">
@@ -9,6 +26,11 @@ const Home = () => {
                 <div className="hero-text">
                     <h1 className="home-title">WELCOME TO RECIPE REALM!</h1>
                 </div>
+            </div>
+            <About />
+            <div className="home__recipes">
+                <h2>Explore Some Delicious Recipes</h2>
+                <RecipeList recipes={recipes} />
             </div>
         </div>
     );
